5608c1fd1ff1e6612b57cd9262924cfcea40f045
Pro100Den07 <drozhkov07@gmail.com>
2024-01-19 19:52:42 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
+//git init
+//git add .
+//git commit -m "first commit"
+//git remote add origin https://github.com/Pro100Den07/hw1.git
+//git push -u origin main
