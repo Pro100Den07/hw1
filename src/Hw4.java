f3f2c56d7eb01c4503d6266cbf2fb99d8aaa4e19
Pro100Den07 <drozhkov07@gmail.com>
2024-01-30 18:43:00 +0200

    first commit

---
+++ src/Hw4.java
@@ -0,0 +1,19 @@
+//2
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 90; i += 5) {
+            System.out.println(i);
+        }
+    }
+}
+//3
+
+
+       public static void main(String[] args) {
+             for (int i = 1; i < 129; i *= 2) {
+                  System.out.println(i);
+             }
+       }
+
+
+
